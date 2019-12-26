7e7d55b6e269d76b86e4cd8b12e827e8952c1035
Robert Soultanaev <robert.soultanaev@gmail.com>
2019-12-26 14:21:47 -0800

    Add more packages

--- configure_ubuntu.sh
+++ configure_ubuntu.sh
@@ -8,13 +8,18 @@ sudo add-apt-repository ppa:snwh/pulp
 
 sudo apt-get update
 sudo apt-get upgrade
-
-sudo apt-get install        \
-    vim                     \
-    git                     \
-    tmux                    \
-    arc-theme               \
-    paper-icon-theme
+sudo apt-get install            \
+    vim                         \
+    git                         \
+    tmux                        \
+    arc-theme                   \
+    paper-icon-theme            \
+    texlive-full                \
+    build-essential             \
+    clang                       \
+    cmake                       \
+    ninja-build                 \
+    python3.8
 
 # Prompt with current working dir
 cat .bashrc >> ~/.bashrc
